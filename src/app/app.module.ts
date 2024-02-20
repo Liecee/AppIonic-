7e32aafe0589dc152cc4f02b1fe420bd0c804885
Liecee <tiliouineliece@live.fr>
2024-02-20 14:31:29 +0100

    Nouveau Projet autre a foiré

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from '@angular/fire/compat'; // Importez AngularFireModule uniquement
+import { environment } from '../environments/environment';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore'; // Import AngularFirestoreModule
+import { AngularFireAuthModule } from '@angular/fire/compat/auth'; // Import AngularFireAuthModule
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // Add AngularFirestoreModule to imports
+    AngularFireAuthModule // Add AngularFireAuthModule to imports
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDem4bleGXi97SCQwzc7yos1lbXzsLZpX8",
+  authDomain: "appionic-6a0a4.firebaseapp.com",
+  projectId: "appionic-6a0a4",
+  storageBucket: "appionic-6a0a4.appspot.com",
+  messagingSenderId: "542462237905",
+  appId: "1:542462237905:web:23afc066f9cc78070ef9f0"
+
+  }
 };
 
 /*
